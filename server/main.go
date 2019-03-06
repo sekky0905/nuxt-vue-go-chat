fff9d353c914ba82a848257cfebaea7ab8631e6d
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-06 23:51:13 +0900

    Add ServeStaticFile func

--- server/main.go
+++ server/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/sekky0905/nuxt-vue-go-chat/server/infra/router"
+)
 
 func main() {
-	r := Sum(2, 3)
-	fmt.Println(r)
+	// for static file
+	entryPoint := "../client/nuxt-vue-go-chat/dist/index.html"
+	router.Router.Path("/").HandlerFunc(ServeStaticFile(entryPoint))
+	router.Router.PathPrefix("/_nuxt/").Handler(http.StripPrefix("/_nuxt/", http.FileServer(http.Dir("../client/nuxt-vue-go-chat/dist/_nuxt/"))))
+
+	http.ListenAndServe(":8080", router.Router)
+}
+
+// ServeStaticFile is deliver static files.
+func ServeStaticFile(entryPoint string) func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, entryPoint)
+	}
+	return http.HandlerFunc(fn)
 }

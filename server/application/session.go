9390d1e5b3e797570710eb07ca7f6e69b552f3d7
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-10 20:29:46 +0900

    Add createSession

--- server/application/session.go
+++ server/application/session.go
@@ -1,7 +1,13 @@
 package application
 
 import (
-	"github.com/sekky0905/go-vue-chat/server/domain/repository"
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/model"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/repository"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/service"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/util"
 )
 
 // SessionService is the interface of SessionService.
@@ -11,15 +17,38 @@ type SessionService interface {
 // sessionService is the service of user.
 type sessionService struct {
 	m        repository.SQLManager
-	sFactory repository.SessionRepoFactory
+	sFactory service.SessionRepoFactory
 	txCloser CloseTransaction
 }
 
 // NewSessionService generates and returns NewSessionService.
-func NewSessionService(m repository.SQLManager, f repository.SessionRepoFactory, txCloser CloseTransaction) SessionService {
+func NewSessionService(m repository.SQLManager, f service.SessionRepoFactory, txCloser CloseTransaction) SessionService {
 	return &sessionService{
 		m:        m,
 		sFactory: f,
 		txCloser: txCloser,
 	}
+}
+
+// createSession creates the session.
+func createSession(ctx context.Context, userID uint32, db repository.DBManager, repo repository.SessionRepository, sService service.SessionService) (*model.Session, error) {
+	session := model.NewSession(userID)
+	session.ID = util.UUID()
+
+	// ready for collision of UUID.
+	yes := true
+	var err error
+	for yes {
+		yes, err = sService.IsAlreadyExistID(ctx, session.ID)
+		if err != nil {
+			if _, ok := errors.Cause(err).(*model.NoSuchDataError); !ok {
+				return nil, errors.Wrap(err, "failed to check whether already exists id or not")
+			}
+		}
+	}
+
+	if err := repo.InsertSession(db, session); err != nil {
+		return nil, errors.Wrap(err, "failed to insert session")
+	}
+	return session, nil
 }

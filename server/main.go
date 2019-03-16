ee6ef5fb47d4f63d9895580c348cf1c078c864d7
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-16 16:23:52 +0900

    Add initializeAuthenticationController

--- server/main.go
+++ server/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"net/http"
 
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/service"
+
+	"github.com/sekky0905/nuxt-vue-go-chat/server/application"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/repository"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/infra/db"
 	"github.com/sekky0905/nuxt-vue-go-chat/server/infra/router"
+	"github.com/sekky0905/nuxt-vue-go-chat/server/interface/controller"
 )
 
 func main() {
+	dbManager := db.NewDBManager()
+
+	apiRouter := router.Router.PathPrefix("/v1/").Subrouter()
+
+	aController := initializeAuthenticationController(dbManager)
+	apiRouter.HandleFunc("/signUp", aController.SignUp).Methods(http.MethodPost)
 
 	// for static file
 	entryPoint := "../client/nuxt-vue-go-chat/dist/index.html"
@@ -24,4 +36,20 @@ func ServeStaticFile(entryPoint string) func(w http.ResponseWriter, r *http.Requ
 		http.ServeFile(w, r, entryPoint)
 	}
 	return http.HandlerFunc(fn)
+}
+
+// initializeAuthenticationController generates and returns AuthenticationController.
+func initializeAuthenticationController(m repository.DBManager) controller.AuthenticationController {
+	txCloser := db.CloseTransaction
+
+	uRepo := db.NewUserRepository()
+	sRepo := db.NewSessionRepository()
+	uService := service.NewUserService(m, uRepo)
+	sService := service.NewSessionService(m, sRepo)
+
+	di := application.NewAuthenticationServiceDIInput(uRepo, sRepo, uService, sService)
+	aApp := application.NewAuthenticationService(m, di, txCloser)
+
+	rm := router.NewRequestManager()
+	return controller.NewAuthenticationController(rm, aApp)
 }

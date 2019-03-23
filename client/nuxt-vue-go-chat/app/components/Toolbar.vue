70e23fb854d1cd1dab9b12f1134fdc77fd7b30f4
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-23 13:44:53 +0900

    Add Base Layout

---
+++ client/nuxt-vue-go-chat/app/components/Toolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar app fixed clipped-left>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>Application</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar'
+}
+</script>
+
+<style scoped></style>

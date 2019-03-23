782c7963b01165de54a5d25f90e19372618310c3
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-23 14:50:00 +0900

    Add SignUp component

--- client/nuxt-vue-go-chat/nuxt.config.js
+++ client/nuxt-vue-go-chat/nuxt.config.js
@@ -38,7 +38,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/vuetify'],
+  plugins: ['@/plugins/vuetify', '@/plugins/vuelidate'],
 
   /*
    ** Nuxt.js modules

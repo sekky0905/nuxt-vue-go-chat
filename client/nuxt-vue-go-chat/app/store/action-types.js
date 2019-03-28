df25c667c053dbfd65333c1a5aa489338921437b
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-28 23:14:26 +0900

    Add action-types about thread

--- client/nuxt-vue-go-chat/app/store/action-types.js
+++ client/nuxt-vue-go-chat/app/store/action-types.js
@@ -1,3 +1,11 @@
 // index.js
 export const LOGIN = 'LOGIN'
 export const SIGN_UP = 'SIGN_UP'
+
+// threads.js
+export const LIST_THREADS = 'LIST_THREADS'
+export const LIST_THREADS_MORE = 'LIST_THREADS_MORE'
+export const SAVE_THREAD = 'SAVE_THREAD'
+export const EDIT_THREAD = 'EDIT_THREAD'
+export const DELETE_THREAD = 'DELETE_THREAD'
+export const CHANGE_IS_DIALOG_VISIBLE = 'CHANGE_IS_DIALOG_VISIBLE'

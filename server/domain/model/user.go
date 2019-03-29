2c028a1a3d52169f9d4ecec6da8d90b9f10636a2
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-30 00:06:27 +0900

    Add User.MarshalLogObject

--- server/domain/model/user.go
+++ server/domain/model/user.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"time"
+
+	"go.uber.org/zap/zapcore"
 )
 
 // User is User model.
@@ -12,4 +14,15 @@ type User struct {
 	Password  string    `json:"password" binding:"required"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// MarshalLogObject for zap logger.
+func (u User) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddInt32("id", int32(u.ID))
+	enc.AddString("name", u.Name)
+	enc.AddString("sessionID", u.SessionID)
+	enc.AddString("password)", u.Password)
+	enc.AddTime("email", u.CreatedAt)
+	enc.AddTime("created_at", u.CreatedAt)
+	return nil
 }

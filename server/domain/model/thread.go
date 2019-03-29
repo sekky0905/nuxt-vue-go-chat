37d7d610b7dce0c3de36b7284cb61f773ad41699
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-30 00:20:28 +0900

    Add ThreadList.MarshalLogObject

--- server/domain/model/thread.go
+++ server/domain/model/thread.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -19,7 +20,7 @@ type Thread struct {
 func (t Thread) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddInt32("id", int32(t.ID))
 	enc.AddString("title", t.Title)
-	if err := enc.AddObject("sessionID", t.User); err != nil {
+	if err := enc.AddObject("user", t.User); err != nil {
 		return err
 	}
 	enc.AddTime("created_at", t.CreatedAt)
@@ -32,4 +33,20 @@ type ThreadList struct {
 	Threads []*Thread `json:"threads"`
 	HasNext bool      `json:"hasNext"`
 	Cursor  uint32    `json:"cursor"`
+}
+
+// MarshalLogObject for zap logger.
+func (t ThreadList) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	zap.Array("threads", zapcore.ArrayMarshalerFunc(func(inner zapcore.ArrayEncoder) error {
+		for _, t := range t.Threads {
+			if err := enc.AddObject("thread", t); err != nil {
+				return err
+			}
+		}
+		return nil
+	}))
+
+	enc.AddBool("hasNext", t.HasNext)
+	enc.AddInt32("cursor", int32(t.Cursor))
+	return nil
 }

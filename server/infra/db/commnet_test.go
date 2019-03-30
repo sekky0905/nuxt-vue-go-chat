e8b3d1d814bd605cd596cb7c5a7b341a6be974fb
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-03-30 19:43:07 +0900

    Add ErrorMsg test

--- server/infra/db/commnet_test.go
+++ server/infra/db/commnet_test.go
@@ -5,6 +5,9 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/pkg/errors"
+
+	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/model"
 	"github.com/sekky0905/nuxt-vue-go-chat/server/domain/repository"
 )
 
@@ -30,6 +33,49 @@ func TestNewCommentRepository(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewCommentRepository(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewCommentRepository() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_commentRepository_ErrorMsg(t *testing.T) {
+	type fields struct {
+		ctx context.Context
+	}
+	type args struct {
+		method model.RepositoryMethod
+		err    error
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr *model.RepositoryError
+	}{
+		{
+			name: "When given appropriate args, returns appropriate error",
+			fields: fields{
+				ctx: context.Background(),
+			},
+			args: args{
+				method: model.RepositoryMethodInsert,
+				err:    errors.New(model.ErrorMessageForTest),
+			},
+			wantErr: &model.RepositoryError{
+				BaseErr:                     errors.New(model.ErrorMessageForTest),
+				RepositoryMethod:            model.RepositoryMethodInsert,
+				DomainModelNameForDeveloper: model.DomainModelNameCommentForDeveloper,
+				DomainModelNameForUser:      model.DomainModelNameCommentForUser,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := &commentRepository{}
+			if err := repo.ErrorMsg(tt.args.method, tt.args.err); errors.Cause(err).Error() != tt.wantErr.Error() {
+				t.Errorf("commentRepository{ErrorMsg() error = %#v, wantErr %#v", err, tt.wantErr)
 			}
 		})
 	}

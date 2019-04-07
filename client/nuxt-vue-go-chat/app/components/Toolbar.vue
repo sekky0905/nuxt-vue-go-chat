291bdee84d6470e1898d5c14d35ee9597fe862a7
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-04-07 21:29:08 +0900

    Fix tooklbar

--- client/nuxt-vue-go-chat/app/components/Toolbar.vue
+++ client/nuxt-vue-go-chat/app/components/Toolbar.vue
@@ -1,16 +1,36 @@
 <template>
-  <v-toolbar app fixed clipped-left>
-    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-    <v-toolbar-title style="color: #64B5F6">Nuxt Go Vue Chat</v-toolbar-title>
-  </v-toolbar>
+  <div>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list dense>
+        <v-list-tile @click="logout()">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title style="color: #64B5F6">Nuxt Go Vue Chat</v-toolbar-title>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Toolbar',
   data: () => ({
-    drawer: true
-  })
+    drawer: false
+  }),
+  methods: {
+    logout() {
+      // TODO implement logput
+      console.log('logout')
+    }
+  }
 }
 </script>
 
